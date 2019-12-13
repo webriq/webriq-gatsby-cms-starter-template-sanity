17ffebdffbe1b9a0fb072fbc8f6d1e3ba7b4202f
Mark Gaudiano <markgaudiano@Marks-iMac.local>
2019-12-13 16:17:45 +0800

    add feed and sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `Gatsby + Sanity`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    siteUrl: `https://webriq-gatsby-cms-starter-template-sanity.webriq.me/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -14,6 +15,64 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allSanityPost } }) => {
+              return allSanityPost.edges.map(edge => {
+                return Object.assign({}, edge.node, {
+                  title: edge.node.title,
+                  description: edge.node.excerpt,
+                  date: edge.node.publishedAt,
+                  url: site.siteMetadata.siteUrl + "/" + edge.node.slug.current,
+                  guid:
+                    site.siteMetadata.siteUrl + "/" + edge.node.slug.current,
+                  custom_elements: [{ "content:encoded": edge.node.body }],
+                })
+              })
+            },
+            query: `
+              {
+                allSanityPost(
+                  filter: { status: { eq: "published" } }
+                  sort: { order: ASC, fields: publishedAt }
+                ) {
+                  edges {
+                    node {
+                      id
+                      title
+                      publishedAt
+                      excerpt
+                      slug {
+                        current
+                      }
+                      body
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Gatsby + Sanity CMS",
+          },
+        ],
+      },
+    },
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-sass`,
@@ -34,8 +93,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Sanity CMS Template`,
+        short_name: `sanitytemplate`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
