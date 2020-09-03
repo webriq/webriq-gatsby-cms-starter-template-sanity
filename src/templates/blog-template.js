69731c90cd82be14f0de63f2182129ba02eadd5a
Dorell James <galangdj@gmail.com>
2020-09-03 11:26:57 +0800

    Fix template site preview error when emptied

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -38,7 +38,7 @@ class BlogTemplate extends React.Component {
               <div className="col-md-9">
                 <div
                   dangerouslySetInnerHTML={{
-                    __html: marked(post._rawBody || []),
+                    __html: marked(post._rawBody || ""),
                   }}
                 />
               </div>
